3dcac3f5aaee9a6dc2174e26c370e2fe72e1e506
topzp <340221700@qq.com>
2018-06-29 15:59:40 +0800

    开奖时间调整
    开奖时间调整

--- docs/js/activity.js
+++ docs/js/activity.js
@@ -31,29 +31,8 @@ $(".Rule #close").click(function () {
     window.location.href = "activity.html";
 });
 
-// 转换时间格式
-function CurentTime()
-{
-    var now = new Date();
-    var year = now.getFullYear();       //年
-    var month = now.getMonth() + 1;     //月
-    var day = now.getDate();            //日
-    var hh = now.getHours();            //时
-    var mm = now.getMinutes();          //分
-    var clock = year + "-";
-    if(month < 10)
-        clock += "0";
-    clock += month + "-";
-    if(day < 10)
-        clock += "0";
-    clock += day + " ";
-    return(clock);
-}
-console.log(CurentTime(myDate.toLocaleDateString()));
-var newtime = CurentTime(myDate.toLocaleDateString());
-console.log(newtime);
-
-tab(newtime,'2018-07-17');
+tab(new Date(),new Date('2018-7-18 14:00:00'));
+console.log(new Date());
 
 //判断页面打开终端
 window.onload = function browserRedirect() {
